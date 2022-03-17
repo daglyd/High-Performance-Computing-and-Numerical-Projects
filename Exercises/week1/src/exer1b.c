e55703caf5e9af1d76b8bfadacdcc28ff4844ddc
daglyd <43292959+daglyd@users.noreply.github.com>
2022-03-17 19:49:30 +0100

    update

--- Exercises/week1/src/exer1b.c
+++ Exercises/week1/src/exer1b.c
@@ -1,47 +1,52 @@
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
 
-void populate_array(int *array, int d);
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+void populate_array(int *array, int array_lenght);
+void find_min_max(int *array, int array_lenght, int *min, int *max);
 
 int main(){
     
     int array_lenght, *array, min, max;
-//    int *array;
-    
     printf("Insert lenght of array: \n"); 
-    scanf("%d",array_lenght);
+    scanf("%d",&array_lenght);
   
-    array =  malloc(lenght * sizeof(int)); 
+    array =  malloc(array_lenght * sizeof(int)); 
     
-    printf("Lenght of array is: %d\n",sizeof(n));
+    populate_array(array, array_lenght); 
+    find_min_max(array, array_lenght, &min, &max);
     
-    printf("Lenght of array is: %d\n",sizeof(&array));
-    populate_array(array, d); 
+    printf("Min value is %d", min);
+    printf("\n");
+    printf("Max value is %d", max); 
     
-    printf("Size of array is: %d\n",sizeof(array));
-    printf("Size of element is: %d\n",sizeof(*array));
-    printf("Lenght of array is: %d\n",sizeof(array)/sizeof(int));
-
 }
 
 
-void populate_array(int *array, int d){
+void populate_array(int *array, int array_lenght){
     
-    for (int i=0; i<d; ++i){
+    for (int i=0; i<array_lenght; ++i){
         array[i] = rand();
     }
     printf("Random numbers:\n-----------------\n");
-    for (int i=0; i<d; i++){
+    for (int i=0; i<array_lenght; i++){
         printf("%d\n", array[i]); 
     }
 }
 
-void find_min_max(int *array){
+void find_min_max(int *array, int array_lenght, int *min, int *max){
     // Finds the min and max values of the array and adds it to pointer
     // variablse *min, *max defined in main() 
-//    for (int i=0; i<lenght
+    int min_, max_;
+    *min, *max  = array[0];
+    for (int i=0; i<array_lenght; i++) {
+        *min = MIN(*min,array[i]);
+        *max = MAX(*max, array[i]);     
+    }
+
 }
